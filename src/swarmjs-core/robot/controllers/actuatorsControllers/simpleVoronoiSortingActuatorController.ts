8084f6174ddacdef68d655c5276ae0ef3278e17a
John Hawley <john@hawley.cc>
2022-03-14 10:31:14 -0230

    Add new grabber behavior for vornoi

---
+++ src/swarmjs-core/robot/controllers/actuatorsControllers/simpleVoronoiSortingActuatorController.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+
+import VoronoiSensor from "../../sensors/voronoi/VoronoiSensor";
+import { polygonContains } from 'd3-polygon';
+import Puck from "../../../puck";
+
+/* eslint-disable prefer-const */
+export default function simpleVoronoiSortingActuatorController(robot, params) {
+  return (sensors, actuators) => {    
+    const closestPuck = sensors.closestPuckToGrabber;
+    const grappedPuck = actuators.grabber.getState() as Puck;
+    const voronoiCell = sensors[VoronoiSensor.name]
+    const position = sensors.position
+
+    if(grappedPuck) {
+      //If either the puck or the robot is outside the vornoi cell, drop the puck
+      if(!polygonContains(voronoiCell, [grappedPuck.position.x, grappedPuck.position.y]) || !polygonContains(voronoiCell, [position.x, position.y])) {
+        actuators.grabber.deactivate();
+      }
+
+      //If the puck is in the goal, drop the puck
+      if (grappedPuck.reachedGoal()) {
+        actuators.grabber.deactivate();
+      }
+    } else {
+      //If not holding puck, pickup closest puck if not in goal and in cell
+      if(closestPuck && !closestPuck.reachedGoal() && polygonContains(voronoiCell, [closestPuck.position.x, closestPuck.position.y])) {
+        actuators.grabber.activate(closestPuck);
+      }
+    }
+  };
+}
