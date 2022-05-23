cd22c7ab97f5c1a6852205d8667767086e4b7b49
John Hawley <john@hawley.cc>
2022-05-23 12:09:16 -0230

    Load the new config

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import * as ReactDOM from 'react-dom';
 import App from './components/App';
 import { exampleConfigs } from './swarmjs-core';
 
-const { simConfig, benchmarkConfig } = exampleConfigs.voronoiSorting;
+const { simConfig, benchmarkConfig } = exampleConfigs.simpleVoronoi;
 
 ReactDOM.render(
   <App config={simConfig} benchSettings={benchmarkConfig}/>,
