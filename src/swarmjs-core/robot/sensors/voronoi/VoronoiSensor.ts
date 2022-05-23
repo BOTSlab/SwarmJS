2f877ae088f34ab14926f40788c658287f71a67f
John Hawley <john@hawley.cc>
2022-05-23 12:14:05 -0230

    code cleanup

--- src/swarmjs-core/robot/sensors/voronoi/VoronoiSensor.ts
+++ src/swarmjs-core/robot/sensors/voronoi/VoronoiSensor.ts
@@ -1,16 +1,16 @@
 import { Delaunay, Voronoi } from "d3-delaunay";
 import Scene from "../../../scene";
 import Robot from "../../robot";
-import { AbstractSensor } from "../sensor";
+import { AbstractSensor, GlobalSensor } from "../sensor";
 import { SensorSamplingType } from "../sensorManager";
 import { GlobalVoronoiSensor } from "./GlobalVoronoiSensor";
 
 
 export class VoronoiCellSensor extends AbstractSensor<Delaunay.Polygon> {
 
-  static sensorName = "voronoiJohnTest";
+  static sensorName = "individualVoronoiSensor";
 
-  private globalSensor: GlobalVoronoiSensor;
+  private globalSensor: GlobalSensor<Robot, Voronoi<Robot>>;
 
 
   constructor(robot: Robot, scene: Scene) {
@@ -29,12 +29,15 @@ export class VoronoiCellSensor extends AbstractSensor<Delaunay.Polygon> {
 
     //Read the global sensor and filter this sensor's value
     const voronoi = this.globalSensor.read().get(this.robot)
+    if (!voronoi) return
 
-    this.value = voronoi?.cellPolygon(this.robot.id)
+    // this.value = voronoi?.cellPolygon(this.robot.id)
+    this.value = voronoi[this.robot.id]
     
     //Use this for all agents to share the same cell
     //Requires change in GlobalVornoiSensor.ts as well
     // this.value = voronoi?.cellPolygon(0)
+
   }
 }
 
