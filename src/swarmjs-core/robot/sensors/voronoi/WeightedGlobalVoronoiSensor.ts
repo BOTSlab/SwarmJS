d598ecc7bc5baebb63a87a43abb8b5d8bce310da
John Hawley <john@hawley.cc>
2023-03-14 20:55:19 -0230

    vornoi cell sensor updates

--- src/swarmjs-core/robot/sensors/voronoi/WeightedGlobalVoronoiSensor.ts
+++ src/swarmjs-core/robot/sensors/voronoi/WeightedGlobalVoronoiSensor.ts
@@ -6,6 +6,10 @@ import { polygonCentroid } from "d3-polygon";
 
 import { weightedVoronoi } from "d3-weighted-voronoi"
 
+
+import { polygonContains } from 'd3';
+import Scene from "../../../scene";
+
 type centroid = {
   x: number,
   y: number,
@@ -24,8 +28,10 @@ export class WeightedGlobalVoronoiSensor extends GlobalSensor<Robot, Voronoi<Rob
 
   public centroids: centroid[]
 
+  private oldWeights: Array<number>
+
   private constructor(scene) {
-    super(scene, WeightedGlobalVoronoiSensor.SENSOR_NAME, SensorSamplingType.onUpdate);
+    super(scene, WeightedGlobalVoronoiSensor.SENSOR_NAME, SensorSamplingType.onUpdate);    
   }
 
   // Get instance of singleton
@@ -60,11 +66,25 @@ export class WeightedGlobalVoronoiSensor extends GlobalSensor<Robot, Voronoi<Rob
 
     this.centroids.sort((a,b) => a.id - b.id)
 
+    
+    let newWeights = Array<number>(this.scene.robots.length).fill(1)
+
+    if(this.voronoi) {
+      newWeights = getWeights(this.voronoi, this.scene)
+    }
+
+    if(!this.oldWeights) {
+      this.oldWeights = newWeights
+    }
+
+    const finalWeights = adjustWeights(this.oldWeights, newWeights)
+    this.oldWeights = finalWeights
+
 
     const centroidsWithWeights: (centroid & hasWeight)[] = []
 
     for(let i = 0; i < this.centroids.length; i++) {
-      centroidsWithWeights.push({x: this.centroids[i].x, y: this.centroids[i].y, id: this.centroids[i].id ,weight: 1000*i })
+      centroidsWithWeights.push({x: this.centroids[i].x, y: this.centroids[i].y, id: this.centroids[i].id, weight: finalWeights[i] })
     }
 
     const weightedVoronoi2 = weightedVoronoi()
@@ -86,8 +106,6 @@ export class WeightedGlobalVoronoiSensor extends GlobalSensor<Robot, Voronoi<Rob
 
     this.voronoi = testing as any
 
-    // this.voronoi = (newVoronoi.reverse()) as any
-
     const ret = new Map<Robot, Voronoi<Robot>>();
   
     this.scene.robots.forEach(robot => {
@@ -96,4 +114,45 @@ export class WeightedGlobalVoronoiSensor extends GlobalSensor<Robot, Voronoi<Rob
 
     return ret
   }
+}
+
+function getWeights(voronoi: Voronoi<Robot>, scene: Scene): number[] {
+
+  const totalPucks: number = scene?.pucks?.length || 0
+  let heldPucks = 0
+  let goalPucks = 0
+  let unheldNotInGoal = 0;
+  const cellScore = scene.robots.map((element, index) => {
+    const voronoiCell = voronoi[index]
+
+    const unheldPucksInCellNotInGoal = scene?.pucks?.filter((p) => {
+      if(p.held && index === 0) { heldPucks++}
+      if(p.reachedGoal() && index === 0) { goalPucks++}
+      if(!p.held && !p.reachedGoal() && index === 0 ) { unheldNotInGoal++}
+
+
+      return (voronoiCell && voronoiCell.length) ? (!p.held && !p.reachedGoal() && polygonContains(voronoiCell, [p.position.x, p.position.y])) : false
+    })
+
+    // const unheldPucks = totalPucks - heldPucks
+    // const notReachedGoalPucks = totalPucks - goalPucks
+
+    const score = unheldPucksInCellNotInGoal?.length || 0
+
+    return (unheldNotInGoal - score) * 6000
+  })
+
+  return cellScore
+}
+function adjustWeights(oldWeights: number[], newWeights: number[]): number[] {
+
+  const WEIGHT_ADJUSTMENT = .05
+  if(oldWeights.length !== newWeights.length) {
+    throw new Error("oldWeights and newWeights must be the same length")
+  }
+
+  return oldWeights.map((oldWeight, index) => {
+    return  oldWeight + ((newWeights[index] - oldWeight) * WEIGHT_ADJUSTMENT)
+  })
+
 }
